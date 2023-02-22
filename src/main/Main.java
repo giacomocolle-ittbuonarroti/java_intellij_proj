7dc0aec4b6f2c359a8afbc8f16dee56fcdf92f9b
giacomocolle-ittbuonarroti <83450223+giacomocolle-ittbuonarroti@users.noreply.github.com>
2023-02-22 15:30:51 +0100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Hello h = new Hello();
+        System.out.println(h.sayHello("Bill");
     }
 }
